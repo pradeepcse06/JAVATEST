e2c7e1313d3f31c5aea23b4069e3544365d7cbc3
Pradeep <pradeepcse06@gmail.com>
2021-03-19 22:49:21 +0530

    Initial commit

---
+++ src/packagea/ClassA.java
@@ -0,0 +1,7 @@
+package packagea;
+
+public class ClassA {
+    public static void main(String[] args) {
+        System.out.println("This Is Package A Class A");
+    }
+}
---
+++ src/packageb/ClassB.java
@@ -0,0 +1,7 @@
+package packageb;
+//testing version control
+public class ClassB {
+    public static void main(String[] args) {
+        System.out.println("This Is Package B Class B");
+    }
+}
