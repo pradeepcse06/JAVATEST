85db1f79f24035dd2f9885af290a4559b45dcfc4
Pradeep <pradeepcse06@gmail.com>
2021-03-19 22:51:52 +0530

    comments

--- src/packageb/ClassB.java
+++ src/packageb/ClassB.java
@@ -1,6 +1,6 @@
 package packageb;
 //testing version control
-//PACKAGE B
+//PACKAGE B testing
 public class ClassB {
     public static void main(String[] args) {
         System.out.println("This Is Package B Class B");
